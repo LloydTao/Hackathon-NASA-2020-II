ff13c86a9957cdab16914bdb32a44f316d851a26
LloydTao <LL23@live.co.uk>
2020-10-04 13:45:28 +0100

    ✨ Add model relationships

--- backend/event/models.py
+++ backend/event/models.py
@@ -1,5 +1,12 @@
 from django.db import models
+from django.utils.timezone import now
+
+from schedule.models import Schedule
 
 
 class Event(models.Model):
-    title = models.CharField(max_length=200)
+    schedule = models.ForeignKey(
+        Schedule, on_delete=models.CASCADE, null=True, blank=False
+    )
+    start_time = models.DateTimeField(default=now, null=True, blank=False)
+    end_time = models.DateTimeField(default=now, null=True, blank=False)
--- backend/schedule/models.py
+++ backend/schedule/models.py
@@ -1,5 +1,16 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.timezone import now
+
+from program.models import Program
 
 
 class Schedule(models.Model):
-    title = models.CharField(max_length=200)
+    user = models.ForeignKey(
+        get_user_model(), on_delete=models.CASCADE, null=True, blank=False
+    )
+    program = models.ForeignKey(
+        Program, on_delete=models.CASCADE, null=True, blank=False
+    )
+    start_date = models.DateTimeField(default=now, null=True, blank=False)
+    end_date = models.DateTimeField(default=now, null=True, blank=False)
