0efa43e80ec22f9ddb355ac8066dbc86a70164c3
LloydTao <LL23@live.co.uk>
2020-10-04 20:36:56 +0100

    Revert "Revert "Merge branch 'feat/schedule-app' of https://github.com/LloydTao/Hackathon-NASA-2020-II into feat/schedule-app""
    This reverts commit e4829794e22ce5adf6bcba3d409355e459e72e19.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "accounts.apps.AccountsConfig",
+    "event.apps.EventConfig",
     "program.apps.ProgramConfig",
     "schedule.apps.ScheduleConfig",
     "rest_framework",
--- backend/event/models.py
+++ backend/event/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils.timezone import now
 
-# Create your models here.
+from schedule.models import Schedule
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=200, null=True, blank=False)
+    type = models.CharField(
+        choices=(
+            ("Sleep", "Sleep"),
+            ("Eat", "Eat"),
+            ("Exercise", "Exercise"),
+            ("Nap", "Nap"),
+            ("Leisure", "Leisure"),
+            ("Other", "Other"),
+        ),
+        default="Sleep",
+        max_length=20,
+    )
+    schedule = models.ForeignKey(
+        Schedule, on_delete=models.CASCADE, null=True, blank=False
+    )
+    start_time = models.DateTimeField(default=now, null=True, blank=False)
+    end_time = models.DateTimeField(default=now, null=True, blank=False)
+
+    def __str__(self):
+        return str(self.title) + " >> " + str(self.schedule)
