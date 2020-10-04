8d9f3e1e3be94c4315c1737315111452079ade57
matt-drayton <47218587+matt-drayton@users.noreply.github.com>
2020-10-04 20:02:33 +0100

    feat: add jwt auth and registration (#8)
    * ✨ Add registration and JWT sessions to project
    
    * ✨ Add tests for logging in
    
    * ✨ Add logging out
    
    * ✨ Config Axios and complete JWT token
    
    * ✨ add logout unit test
    
    * ✨ Lint backend
    
    * 🎨 Remove redundant comment, re-order urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -22,6 +22,7 @@ from accounts.urls import router as accounts_router
 from program.urls import router as program_router
 from schedule.urls import router as schedule_router
 
+from accounts.views import LoginView, LogoutView
 
 router = routers.DefaultRouter()
 router.registry.extend(accounts_router.registry)
@@ -30,5 +31,7 @@ router.registry.extend(schedule_router.registry)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/login/", LoginView.as_view(), name="login"),
+    path("api/logout/", LogoutView.as_view(), name="logout"),
     path("api/", include(router.urls)),
 ]
---
+++ frontend/components/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <input v-model="username" type="text" placeholder="Username" />
+    <input v-model="password" type="password" placeholder="Password" />
+    <button @click="login()">Log In</button>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      username: '',
+      password: '',
+    }
+  },
+  methods: {
+    login() {
+      this.$axios.post('/api/login/', {
+        username: this.username,
+        password: this.password,
+      })
+    },
+  },
+}
+</script>
